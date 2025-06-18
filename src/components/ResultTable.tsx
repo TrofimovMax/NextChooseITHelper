b76565bdc7b36414d6a1c38e6e5d9bacfeff5fb3
trofimovmax <92295615+TrofimovMax@users.noreply.github.com>
2025-06-18 23:35:15 +0300

    improvement: improves the percentage display of results

--- src/components/ResultTable.tsx
+++ src/components/ResultTable.tsx
@@ -7,6 +7,7 @@ import Grid from "@mui/material/Grid2";
 interface ResultItem {
   name: string;
   language_name: string;
+  framework_title: string;
   smart_score?: number;
   ahp_score?: number;
   awm_score?: number;
@@ -31,19 +32,22 @@ export default function ResultTable({ title, results }: ResultTableProps) {
               <Typography variant="body2">
                 Язык: {item.language_name}
               </Typography>
+              <Typography variant="body2">
+                Фреймворк: {item.framework_title}
+              </Typography>
               {item.smart_score !== undefined && (
                 <Typography variant="body2">
-                  SMART Score: {item.smart_score.toFixed(5)}
+                  SMART Score: {(item.smart_score * 100).toFixed(2)} %
                 </Typography>
               )}
               {item.ahp_score !== undefined && (
                 <Typography variant="body2">
-                  AHP Score: {item.ahp_score.toFixed(5)}
+                  AHP Score: {(item.ahp_score  * 100).toFixed(2)} %
                 </Typography>
               )}
               {item.awm_score !== undefined && (
                 <Typography variant="body2">
-                  AWM Score: {item.awm_score.toFixed(5)}
+                  AWM Score: { (item.awm_score * 100).toFixed(2)} %
                 </Typography>
               )}
             </Paper>
